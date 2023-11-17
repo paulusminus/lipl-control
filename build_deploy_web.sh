963ff185a3193c2d191e55a26134614a04e64a62
paulusminus <info@paulmin.nl>
2023-11-17 13:34:04 +0100

    rm unneeded

--- build_deploy_web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-flutter pub get
-flutter build web --release --source-maps
-ssh paul@www.paulmin.nl 'rm -rf /var/www/html/lipl.paulmin.nl/*'
-rsync -avz build/web/* paul@www.paulmin.nl:/var/www/html/lipl.paulmin.nl/
