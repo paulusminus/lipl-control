eba076c3aad47de92f22617ec86d5af27017216a
paulusminus <info@paulmin.nl>
2024-05-26 10:16:51 +0200

    Version 1.10.3

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,13 @@ import 'package:lipl_control/app/view/providers.dart';
 import 'package:lipl_control/bloc_observer.dart';
 import 'package:logging/logging.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:universal_io/io.dart';
 
 Future<void> main() async {
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen(
     (LogRecord record) {
-      stdout.writeln(
+      // ignore: avoid_print
+      print(
         '${record.level.name}: ${record.time}: ${record.message}',
       );
     },
